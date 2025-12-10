097e0a71c4ab3b4136894ea1f4ba63fd735d873d
Traffelet <benjamin@traffelet.com>
2025-12-10 23:56:19 +0100

    // Task & Assignement Ergänzungen

--- backend/src/main/java/com/example/shiftplanner/api/task/dto/TaskAssignmentCreateDto.java
+++ backend/src/main/java/com/example/shiftplanner/api/task/dto/TaskAssignmentCreateDto.java
@@ -1,14 +1,22 @@
 package com.example.shiftplanner.api.task.dto;
 
 import com.example.shiftplanner.domain.task.TimeRange;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskAssignmentCreateDto {
+
+    @NotNull
     private Long taskId;
+
+    @NotNull
     private Long staffId;
+
     private TimeRange timeRange;
+
 }
