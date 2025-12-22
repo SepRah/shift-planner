ee4d944c6993bc0a9834afec1a114f5d3c34861d
Traffelet <benjamin@traffelet.com>
2025-12-22 16:55:10 +0100

    // App.jsx ergänzt mit der Planner Seite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import RegistrationSuccessPage from "./pages/RegistrationSuccesspage.jsx";
+import PlannerPage from "./pages/PlannerPage.jsx";
 
 function App() {
     return (
@@ -14,6 +15,9 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/register/success" element={<RegistrationSuccessPage />} />
+
+                {/* Planner route */}
+                <Route path="/planner" element={<PlannerPage />} />
 
                 {/* Optional: 404 page */}
                 <Route path="*" element={<div>Page not found</div>} />
