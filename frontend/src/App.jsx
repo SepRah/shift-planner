90e20e886807a2fe1c58d08ff410f078d8c23203
SepRah <cybersepehr@gmail.com>
2026-01-09 13:25:01 +0100

    Task list page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ import UserDashboard from "./pages/UserDashboard.jsx";
 import UnauthorizedPage from "./pages/UnauthorizedPage.jsx";
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import ManagementLayout from "./routes/ManagementLayout.jsx";
-import ManagementPage from "./pages/UsersManagementPage.jsx";
 import UsersManagementPage from "./pages/UsersManagementPage.jsx";
 import TaskManagementPage from "./pages/TaskManagementPage.jsx";
 
